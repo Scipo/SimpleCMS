8352856295b222576fdf64edb0ea16fd73c97508
Scipo <ivots@mail.bg>
2017-12-04 20:56:51 +0200

    create editor now works

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -41,7 +41,7 @@
     {!! Form::close() !!}
     @endsection
 @section('script')
-<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+<script src="{{asset('vendor/unisharp/laravel-ckeditor/ckeditor.js')}}"></script>
 <script>
     CKEDITOR.replace( 'article-ckeditor' );
 </script>
